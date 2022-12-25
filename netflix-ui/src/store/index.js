bee252b564f8f19314d9ae3bcfa9f2caffef442a
elijah <elijahkungu100@gmail.com>
2022-12-25 23:08:16 +0300

    api TMD

--- netflix-ui/src/store/index.js
+++ netflix-ui/src/store/index.js
@@ -1,4 +1,8 @@
+import { async } from "@firebase/util";
 import {  configureStore, createAsyncThunk,createSlice} from "@reduxjs/toolkit";
+import axios from 'axios';
+import { API_KEY, TMDB_URL } from "../utils/constants";
+
 const initialState = {
     movies:[],
     genresLoaded:false,
@@ -9,6 +13,13 @@ const NetflixSlice = createSlice({
     initialState,
     extraReducers:(builder) =>{}, 
 });
+export const getGenres = createAsyncThunk("netflix/genres", async() => {
+    const {data}  = await axios.get(
+        `${TMDB_URL}/genre/movie/list?api_key=${API_KEY}`
+    );
+    // return genres;
+    console.log(data)
+})
 
 export const store = configureStore({
     reducer: {
